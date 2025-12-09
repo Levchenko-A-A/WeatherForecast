4f6a8bad6512661fe3dcfea6df67e18f9181cdb1
Андрей Левченков <lev_a_a@mail.ru>
2025-12-09 19:09:53 +0200

    Lint check & fix

--- src/createMainSection.js
+++ src/createMainSection.js
@@ -1,37 +1,37 @@
-import { createCurrentWeatherSection } from "./createCurrentWeatherSection"
+import { createCurrentWeatherSection } from "./createCurrentWeatherSection";
 
-export function createMainSection (el) {
-    const appContainer = document.createElement('div');
-    appContainer.className = 'container';
-    const header = document.createElement('header');
-    const title = document.createElement('h1');
-    title.textContent = 'Прогноз погоды';
-    const searchConteiner = document.createElement('div');
-    searchConteiner.className = 'search-container';
+export function createMainSection(el) {
+  const appContainer = document.createElement("div");
+  appContainer.className = "container";
+  const header = document.createElement("header");
+  const title = document.createElement("h1");
+  title.textContent = "Прогноз погоды";
+  const searchConteiner = document.createElement("div");
+  searchConteiner.className = "search-container";
 
-    const cityInput = document.createElement('input');
-    cityInput.type = 'text';
-    cityInput.className = 'city-input';
-    cityInput.placeholder = 'Введите название города... (например Москва)';
-    
-    const searchButton = document.createElement('button');
-    searchButton.className = 'search-btn';
-    searchButton.textContent = 'Поиск';
+  const cityInput = document.createElement("input");
+  cityInput.type = "text";
+  cityInput.className = "city-input";
+  cityInput.placeholder = "Введите название города... (например Москва)";
 
-    searchConteiner.append(cityInput, searchButton);
-    header.append(title, searchConteiner);
+  const searchButton = document.createElement("button");
+  searchButton.className = "search-btn";
+  searchButton.textContent = "Поиск";
 
-    const main = document.createElement('main');
-    const weatherContainer = document.createElement('div');
-    weatherContainer.className = 'weather-container';
-    const loadingElement = document.createElement('div');
-    loadingElement.className = 'loading';
-    // loadingElement.textContent = 'Загрузка данных о погоде...';
+  searchConteiner.append(cityInput, searchButton);
+  header.append(title, searchConteiner);
 
-    const currentWeather = createCurrentWeatherSection();
+  const main = document.createElement("main");
+  const weatherContainer = document.createElement("div");
+  weatherContainer.className = "weather-container";
+  const loadingElement = document.createElement("div");
+  loadingElement.className = "loading";
+  // loadingElement.textContent = 'Загрузка данных о погоде...';
 
-    weatherContainer.append(loadingElement, currentWeather);
-    main.append(weatherContainer);
-    appContainer.append(header, main);
-    el.append(appContainer);
+  const currentWeather = createCurrentWeatherSection();
+
+  weatherContainer.append(loadingElement, currentWeather);
+  main.append(weatherContainer);
+  appContainer.append(header, main);
+  el.append(appContainer);
 }
--- src/runApp.js
+++ src/runApp.js
@@ -1,8 +1,8 @@
 import { createMainSection } from "./createMainSection";
-import { setupEventListners} from "./setupEventListners";
+import { setupEventListners } from "./setupEventListners";
 import "./runApp.css";
 
 export function runApp(el) {
-    createMainSection(el);
-    setupEventListners();
-};
+  createMainSection(el);
+  setupEventListners();
+}
